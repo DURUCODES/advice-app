0b8009fc12d8b4693071a997eff41086e35f67a9
KINGSLEY DURUCHUKWU <142257235+DURUCODES@users.noreply.github.com>
2024-07-05 01:54:19 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+async function getAdvice (){
+     const url = await fetch('https://api.adviceslip.com/advice');
+     const data= await url.json()
+     console.log(data)
+     const adviceNumber= document.querySelector('.number')
+     const adviceBody = document.querySelector('.advice')
+
+     adviceNumber.textContent=`Advice:${data.slip.id}`
+     adviceBody.textContent= `""${data.slip.advice}`
+}
+
+
+
+
+document.querySelector('.dicebtn').addEventListener('click', getAdvice)
